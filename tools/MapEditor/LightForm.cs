e8b563781373c6d28678a1caeac3eee19f6f3c60
Austin Roberge <roberge.austin@gmail.com>
2014-02-13 10:39:35 -0600

    Update LightForm.cs

--- tools/MapEditor/LightForm.cs
+++ tools/MapEditor/LightForm.cs
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with HPL1 Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 using System;
 using System.Drawing;
